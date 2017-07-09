8ed40c1fd970cc134c191837fdb6a67d8e1976eb
MONKEY-D-LUFFY\saile <nhuja08@gmail.com>
2017-07-09 15:43:50 +0545

    add new file

---
+++ src/component/posts_old.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+static class AlertMe extends Component{
+    render(){
+        return (
+            <div>
+                <a href="www.facebook.com">FB</a>
+            </div>
+        );
+    }
+}
+
+export default AlertMe;
